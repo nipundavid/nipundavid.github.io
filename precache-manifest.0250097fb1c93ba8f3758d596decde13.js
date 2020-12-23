1209a7ec5415b0d19934bf669f207118c8ac3ef0
Nipun David <nipun18david@gmail.com>
2020-12-23 14:59:55 +0530

    first commit

---
+++ precache-manifest.0250097fb1c93ba8f3758d596decde13.js
@@ -0,0 +1,306 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3d19228b899d7375b4f5a5cb98a84d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f36f3a5a4b450b14d5a9",
+    "url": "/static/css/main.ba6287c2.chunk.css"
+  },
+  {
+    "revision": "4a219fe558695fc2358e",
+    "url": "/static/js/2.74038721.chunk.js"
+  },
+  {
+    "revision": "b2c0fc6cec225cf6fa24000af731694f",
+    "url": "/static/js/2.74038721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f36f3a5a4b450b14d5a9",
+    "url": "/static/js/main.f6b8b979.chunk.js"
+  },
+  {
+    "revision": "ff1f69a199eb9e7724e21865c80c05b6",
+    "url": "/static/js/main.f6b8b979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c73d37e4d976189122b",
+    "url": "/static/js/runtime-main.6931da03.js"
+  },
+  {
+    "revision": "94e962101a690bba5ea63039acfc4694",
+    "url": "/static/media/1.94e96210.png"
+  },
+  {
+    "revision": "edf54a14cf18e615a9abdce9c680cc53",
+    "url": "/static/media/2.edf54a14.png"
+  },
+  {
+    "revision": "ec9e8aed6550531b21f7ab8ee5ae5184",
+    "url": "/static/media/3.ec9e8aed.png"
+  },
+  {
+    "revision": "025054163d8cbd32c67162d08d186e09",
+    "url": "/static/media/4.02505416.png"
+  },
+  {
+    "revision": "e698ea4e9486c655a0b38a06151785d2",
+    "url": "/static/media/5.e698ea4e.png"
+  },
+  {
+    "revision": "3497164496cdee8527726b1245b776e6",
+    "url": "/static/media/6.34971644.png"
+  },
+  {
+    "revision": "33baf97f496715a4b20e53b2342290a8",
+    "url": "/static/media/ExpressJS.33baf97f.png"
+  },
+  {
+    "revision": "b0fa380de9033d19cc52a9a67f5755cb",
+    "url": "/static/media/arcore.b0fa380d.png"
+  },
+  {
+    "revision": "63831509201ae42e10658bf57e18c181",
+    "url": "/static/media/arkit.63831509.png"
+  },
+  {
+    "revision": "2a26a851f365665a3df6b59b40713674",
+    "url": "/static/media/babylonjs-1.2a26a851.png"
+  },
+  {
+    "revision": "b5e6d0bed9b80761b360748c030a39ab",
+    "url": "/static/media/babylonjs-2.b5e6d0be.png"
+  },
+  {
+    "revision": "b415b2acac7495d1f1e3d9bf9e9176a9",
+    "url": "/static/media/babylonjs.b415b2ac.png"
+  },
+  {
+    "revision": "d70c756f2a739722a398819cfa7287a7",
+    "url": "/static/media/bitcoin.d70c756f.png"
+  },
+  {
+    "revision": "2e7c815186bc8e23f7864471a22a4dbb",
+    "url": "/static/media/blender.2e7c8151.png"
+  },
+  {
+    "revision": "01899307116a937c66a0eb4b3927b631",
+    "url": "/static/media/blob.01899307.png"
+  },
+  {
+    "revision": "8d581ffb03c56a0106abc6b330b449d5",
+    "url": "/static/media/cercuri.8d581ffb.png"
+  },
+  {
+    "revision": "62a04e2688d8ba7ede0d823e425cf999",
+    "url": "/static/media/chester-wade.62a04e26.jpg"
+  },
+  {
+    "revision": "2ae3f9ecc812ca146d952de9c8e8c0ba",
+    "url": "/static/media/denys.2ae3f9ec.jpg"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "4fd59ead57f5b7dfa3ca0db01414a256",
+    "url": "/static/media/etherum.4fd59ead.png"
+  },
+  {
+    "revision": "182796a27d309b897e77f616df128522",
+    "url": "/static/media/fabien-bazanegue.182796a2.jpg"
+  },
+  {
+    "revision": "2e2571256140872fb7e4f246ea786669",
+    "url": "/static/media/github.2e257125.svg"
+  },
+  {
+    "revision": "08ea438ed3b14e63dd54516d3768e3d3",
+    "url": "/static/media/google.08ea438e.svg"
+  },
+  {
+    "revision": "9086811a9d83ebcf6f33cc2e3c5ae39c",
+    "url": "/static/media/hololens.9086811a.png"
+  },
+  {
+    "revision": "ddaf636676f8b04cdec8cdb40750277f",
+    "url": "/static/media/james.ddaf6366.jpg"
+  },
+  {
+    "revision": "c085e83e15808eff561f13f30ce937c2",
+    "url": "/static/media/landing-page.c085e83e.png"
+  },
+  {
+    "revision": "96764d80118b4ece734dc14401283b76",
+    "url": "/static/media/lora.96764d80.jpg"
+  },
+  {
+    "revision": "3a749d183532590fda1c14345b6ccca6",
+    "url": "/static/media/mark-finn.3a749d18.jpg"
+  },
+  {
+    "revision": "6654dc091639eb03c32d31d3ffd495c7",
+    "url": "/static/media/mike.6654dc09.png"
+  },
+  {
+    "revision": "be4a1748d0f94c35e0a661164495d160",
+    "url": "/static/media/mike.be4a1748.jpg"
+  },
+  {
+    "revision": "fc03ad54bc7720aea7033ae4ab3d1d7c",
+    "url": "/static/media/node.fc03ad54.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b2fe7eb62fe8a893cdfccc7109ab54dd",
+    "url": "/static/media/oculus.b2fe7eb6.png"
+  },
+  {
+    "revision": "b1e01ebbb0d098277dcc97759bc3f402",
+    "url": "/static/media/path1.b1e01ebb.png"
+  },
+  {
+    "revision": "43ff45c5dc1f65726b86fc75ea6cabc0",
+    "url": "/static/media/path2.43ff45c5.png"
+  },
+  {
+    "revision": "a70167ea1491987c50049961a9e2e45f",
+    "url": "/static/media/path3.a70167ea.png"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "ceb241f05718fb1311f8810eea959803",
+    "url": "/static/media/path5.ceb241f0.png"
+  },
+  {
+    "revision": "03ea2a0ad05689cebb5a9314a53b8966",
+    "url": "/static/media/patrat.03ea2a0a.png"
+  },
+  {
+    "revision": "2515c8e5bccbb8334071a0cce1bb2540",
+    "url": "/static/media/profile-page.2515c8e5.png"
+  },
+  {
+    "revision": "1b0cba39c3365e106d347d3a7c9e8a81",
+    "url": "/static/media/react.1b0cba39.png"
+  },
+  {
+    "revision": "98742a820d2986e78ae284d57deae457",
+    "url": "/static/media/ripp.98742a82.png"
+  },
+  {
+    "revision": "9d4a558e7169ed763a05715627bc088c",
+    "url": "/static/media/ryan.9d4a558e.jpg"
+  },
+  {
+    "revision": "75f5eb4947348dc2181457e59d4f30e9",
+    "url": "/static/media/shadow-jump-1.75f5eb49.png"
+  },
+  {
+    "revision": "680aa87a9b7547ce84dbf6d75668b928",
+    "url": "/static/media/shadow-jump-2.680aa87a.png"
+  },
+  {
+    "revision": "0b3a668a417d1bba8a72e47589ba79f8",
+    "url": "/static/media/shadow-run-1.0b3a668a.png"
+  },
+  {
+    "revision": "f7856cd86a99ef91bd875f511f5fe595",
+    "url": "/static/media/sheep-render.f7856cd8.png"
+  },
+  {
+    "revision": "79c1998fb18c2d0078bee11f902364e3",
+    "url": "/static/media/square-purple-1.79c1998f.png"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/static/media/square5.ff004a0e.png"
+  },
+  {
+    "revision": "4be37aae134e305a32bdaf366d5f2494",
+    "url": "/static/media/that-cold-night.4be37aae.png"
+  },
+  {
+    "revision": "9863c8d92d6761662c1fd264cb58f148",
+    "url": "/static/media/thumb_360xp.9863c8d9.png"
+  },
+  {
+    "revision": "7f998bf8600be1521fd75da9e029f5fc",
+    "url": "/static/media/thumb_ColorJump3D.7f998bf8.jpg"
+  },
+  {
+    "revision": "dd5ea84f4111fb3522a18ebdf0c45d5a",
+    "url": "/static/media/thumb_ReUsableUIForUnity3D.dd5ea84f.png"
+  },
+  {
+    "revision": "52c5774794d7703fdee468960a223f7d",
+    "url": "/static/media/thumb_ShadowJump.52c57747.png"
+  },
+  {
+    "revision": "d26d9328b97cab5309c2044e2610dbc3",
+    "url": "/static/media/thumb_ShadowRun.d26d9328.png"
+  },
+  {
+    "revision": "b46a483668d7ac89c458ee95d3508627",
+    "url": "/static/media/thumb_kitchen-designer.b46a4836.png"
+  },
+  {
+    "revision": "178bf12a933db9bc0b6e5598ba176453",
+    "url": "/static/media/unity-screenshot-1.178bf12a.PNG"
+  },
+  {
+    "revision": "7c871f954dd96e23e395a2d85fb4bb37",
+    "url": "/static/media/unity-screenshot-2.7c871f95.PNG"
+  },
+  {
+    "revision": "64af3cffefdc7d1df1de9868f7a70ecd",
+    "url": "/static/media/unity3D.64af3cff.png"
+  },
+  {
+    "revision": "81d0edf6d4208fc11c720f6bf81a90c2",
+    "url": "/static/media/unreal.81d0edf6.png"
+  },
+  {
+    "revision": "9437dd44122dbff57990d95edec5685e",
+    "url": "/static/media/vscode.9437dd44.png"
+  },
+  {
+    "revision": "11fa5f73cb719f0f1a38e02e47bdc843",
+    "url": "/static/media/waves.11fa5f73.png"
+  }
+]);
